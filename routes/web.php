6c3bb8263a4cf21fe215386e3bafeb073300726f
satindergill <satindergill07@yahoo.com>
2017-04-12 02:02:13 +1000

    Second

--- routes/web.php
+++ routes/web.php
@@ -5,20 +5,23 @@
 Route::get('/', function () {
     return view('welcome');
 });
-Route::resource('movies','MovieController');
-
-
-
-Route::get('/celebs',function ()
-{
-    return view('celebs');
-});
-
-
-Route::get('/news' , function ()
-{
-    return view ('news');
-});
+//Route::resource('movies','MovieController');
+//
+//
+//
+//Route::get('/celebs',function ()
+//{
+//    return view('celebs');
+//});
+//
+//
+//Route::get('/news' , function ()
+//{
+//    return view ('news');
+//});
 Auth::routes();
 
 Route::get('/home', 'HomeController@index');
+
+
+Route::resource('admin/users','AdminUsersController');
